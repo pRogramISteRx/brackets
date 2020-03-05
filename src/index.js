509306758f064f426b8167cb7da9d3d8e5ad5a2f
Vadim Kotkovets <kotkowec@gmail.com>
2020-03-05 23:22:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                str[i] === bracketsConfig[j][0] &&
+                bracketsConfig[j][0] !== bracketsConfig[j][1]
+            ) {
+                stack.push(str[i]);
+                break;
+            }
+
+            if (
+                str[i] === bracketsConfig[j][1] &&
+                bracketsConfig[j][0] !== bracketsConfig[j][1]
+            ) {
+                if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+                    stack.pop();
+                    break;
+                }
+            }
+
+            if (
+                str[i] === bracketsConfig[j][0] &&
+                bracketsConfig[j][0] === bracketsConfig[j][1]
+            ) {
+                if (stack[stack.length - 1] !== str[i]) {
+                    stack.push(str[i]);
+                    break;
+                } else stack.pop();
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else return false;
+};
